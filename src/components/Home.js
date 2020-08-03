30c990b63171c0c98814d2de030ab138e5324941
Anelise Newman <apnewman@mit.edu>
2020-08-03 15:29:53 -0700

    Basic sketch of home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import me from "../images/me.jpg";
+
+import "./Home.css";
+import "../global-styles.css";
 
 function HomePage() {
-  return <p>My name is Anelise</p>;
+  return (
+    <div className="vertical-center">
+      <div>
+        <div
+          className="me-pic-container"
+          style={{ backgroundImage: `url(${me})` }}
+        ></div>
+        {/* <img src={me} className="me-pic" alt="Portrait of Anelise Newman" /> */}
+        <p>
+          I'm <b>Anelise Newman</b>. I am a Master of Enginering student at MIT
+          working with Aude Oliva. I am interested in understanding and modeling
+          how humans interact with visual content. My research projects often
+          span computer vision, cognitive science, and HCI. I recently finished
+          my undergraduate at MIT.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
   return (
     <div className="background">
       <Sidebar />
-      {children}
+      <div className="main">{children}</div>
     </div>
   );
 };
