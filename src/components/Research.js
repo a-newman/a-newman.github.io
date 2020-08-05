aa1927e2fda0124e53c536c9329df2826e69ba08
Anelise Newman <apnewman@mit.edu>
2020-08-05 12:38:20 -0700

    yaml and image importing working

--- src/components/Research.js
+++ src/components/Research.js
@@ -1,8 +1,8 @@
 import React from "react";
-import louie from "../images/louie.jpg";
-import * as MdIcons from "react-icons/md";
+// import * as MdIcons from "react-icons/md";
 import * as IoIcons from "react-icons/io";
 import data from "./Research.yaml";
+import louie from "../images/louie.jpg";
 
 import "./Research.css";
 import "../global-styles.css";
@@ -17,7 +17,6 @@ const IconLinkButton = props => {
       <div className="project-link-elt">
         <span>
           <Icon />
-          {/* <icon /> */}
         </span>
         {props.name}
       </div>
@@ -25,52 +24,76 @@ const IconLinkButton = props => {
   );
 };
 
+const Section = props => {
+  return (
+    <div>
+      <h2 className="section-title">
+        <b>{props.title}</b>
+      </h2>
+      {props.children}
+    </div>
+  );
+};
+
 const Publication = props => {
-  console.log("data", data);
+  const authorList = props.authors
+    .map((author, i) => {
+      const isMe = author.includes("Anelise Newman");
+      const key = `author${i}`;
+      return isMe ? <b key={key}>{author}</b> : <span key={key}>{author}</span>;
+    })
+    .reduce((prev, curr) => [prev, ", ", curr]);
+
+  const images = require.context("../images/", true);
+  const imageSrc = images("./" + props.imageSrc);
+
   return (
     <div className="publication-section-container">
       <div>
         <h3 className="project-title">{props.title}</h3>
-        <h5 className="project-authors">{props.authors}</h5>
+        <h5 className="project-authors">{authorList}</h5>
         <h5 className="project-venue">
           <i>{props.venue}</i>
         </h5>
-        <p>
-          <b>Abstract. </b>
-          {props.abstract}
-        </p>
-        <div className="project-links-container">
-          {props.links.map((elt, i) => (
-            <IconLinkButton
-              name={elt.name}
-              iconName={elt.iconName}
-              href={elt.href}
-              key={`iconLinkButton${i}`}
-            />
-          ))}
-        </div>
-        <div className="project-picture">
-          <img src={props.imageSrc} alt={props.imageAlt} />
-        </div>
+        {props.abstract && (
+          <p>
+            <b>Abstract. </b>
+            {props.abstract}
+          </p>
+        )}
+        {props.links && (
+          <div className="project-links-container">
+            {props.links.map((elt, i) => (
+              <IconLinkButton
+                name={elt.name}
+                iconName={elt.iconName}
+                href={elt.href}
+                key={`iconLinkButton${i}`}
+              />
+            ))}
+          </div>
+        )}
+        {props.imageSrc && (
+          <div className="project-picture">
+            <img src={imageSrc} alt={props.imageAlt} />
+          </div>
+        )}
       </div>
     </div>
   );
 };
 
 function ResearchPage() {
-  const links = [{ name: "Louie", iconName: "IoMdDocument", href: louie }];
   return (
-    <div>
-      <Publication
-        title="Sample title"
-        authors="Anelise"
-        venue="ECCV"
-        imageSrc={louie}
-        imageAlt="my dog louie"
-        abstract="here is the abstract for this project about Louie."
-        links={links}
-      />
-    </div>
+    <>
+      {data.sections.map((section, section_i) => (
+        <Section title={section.name} key={`Section${section_i}`}>
+          {section.items.map((item, item_i) => (
+            <Publication {...item} key={`Publication${item_i}`} />
+          ))}
+        </Section>
+      ))}
+    </>
   );
 }
 
