5947f74138b8148b17440475c8d5e9a115c277b5
Anelise Newman <apnewman@mit.edu>
2020-09-09 01:01:49 -0700

    small updates to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function HomePage() {
     { name: "CV", href: { cv } },
     { name: "LinkedIn", href: "https://www.linkedin.com/in/anelise-newman/" },
     { name: "Github", href: "https://github.com/a-newman/" },
-    { name: "Email", href: "mailto:apnewman@mit.edu" }
+    { name: "Email", href: "mailto:anelise@stanford.edu" }
   ];
   return (
     <div className="vertical-center">
@@ -20,11 +20,11 @@ function HomePage() {
         <CircularImage src={me} className="me-pic-container" />
         {/* <img src={me} className="me-pic" alt="Portrait of Anelise Newman" /> */}
         <p>
-          I'm <b>Anelise Newman</b>. I am a Master of Enginering student at MIT
-          working with Aude Oliva. I am interested in understanding and modeling
-          how humans interact with visual content. My research projects often
-          span computer vision, cognitive science, and HCI. I recently finished
-          my undergraduate at MIT.
+          I'm <b>Anelise Newman</b>. I am a 1st-year PhD student at Stanford in
+          Computer Vision and HCI. Previously, I received my MEng at MIT working
+          with Aude Oliva. I am interested in understanding and modeling how
+          humans interact with visual content. My research projects often span
+          computer vision, cognitive science, and HCI.
         </p>
         <SimpleLinkList links={links} />
       </div>
