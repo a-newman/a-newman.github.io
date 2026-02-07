c7b19badcdd6e2c94b2948343e1453522c8dc192
Anelise Newman <anelise@Mac.socal.rr.com>
2026-02-06 19:24:39 -0800

    Delete unused things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
 import Home from "./components/Home.js";
 import Layout from "./components/Layout.js";
-import Personal from "./components/Personal.js";
-import Research from "./components/Research.js";
 
 import "./global-styles.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/research" element={<Research />} />
-            <Route path="/personal" element={<Personal />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <Layout>
+        <Home />
+      </Layout>
     </div>
   );
 }
